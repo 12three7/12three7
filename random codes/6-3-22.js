f61b8335009c5456eb31b20b6733e64e8cf97acc
1237 <92003875+12three7@users.noreply.github.com>
2022-03-06 17:39:42 +0800

    Update 6-3-22.js

--- random codes/6-3-22.js
+++ random codes/6-3-22.js
@@ -15,3 +15,23 @@ console.log(b * c + b / c) // null?
 var d = "anjai"
 var e = true
 console.log(e * d) // null lagi
+
+// ganti console.log jadi print()
+
+var x = 1.5
+var y = true
+print(x + y) // 2.5?
+print(x * x * x) // 3.375, oklah
+print(y / x / x / x) //0.2962962962962963?
+
+var z = false
+var a = "anjai"
+print(z + a) // falseanjai?
+
+var b = true
+var c = false
+print(b * c + b / c) // null?
+
+var d = "anjai"
+var e = true
+print(e * d) // null lagi
