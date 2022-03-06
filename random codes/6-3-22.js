c9c934600baf96da464aa92f03487eb36cd8b3f3
1237 <92003875+12three7@users.noreply.github.com>
2022-03-06 17:42:46 +0800

    Update 6-3-22.js

--- random codes/6-3-22.js
+++ random codes/6-3-22.js
@@ -1,8 +1,10 @@
+// 6/3/22 variable errors
+
 var x = 1.5
 var y = true
 console.log(x + y) // 2.5?
 console.log(x * x * x) // 3.375, oklah
-console.log(y / x / x / x) //0.2962962962962963?
+console.log(y / x / x / x) //0.2962962962962963?, ok bagus
 
 var z = false
 var a = "anjai"
