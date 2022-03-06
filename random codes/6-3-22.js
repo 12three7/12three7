9fd1e776efed80205b43ee2ac70f67950aed93c5
1237 <92003875+12three7@users.noreply.github.com>
2022-03-06 17:38:22 +0800

    Create 6-3-22.js

---
+++ random codes/6-3-22.js
@@ -0,0 +1,17 @@
+var x = 1.5
+var y = true
+console.log(x + y) // 2.5?
+console.log(x * x * x) // 3.375, oklah
+console.log(y / x / x / x) //0.2962962962962963?
+
+var z = false
+var a = "anjai"
+console.log(z + a) // falseanjai?
+
+var b = true
+var c = false
+console.log(b * c + b / c) // null?
+
+var d = "anjai"
+var e = true
+console.log(e * d) // null lagi
