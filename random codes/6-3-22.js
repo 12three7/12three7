2525791f76771a630b5ff3bcfb50d3dc63bf3329
1237 <92003875+12three7@users.noreply.github.com>
2022-03-11 20:46:11 +0800

    Update 6-3-22.js

--- random codes/6-3-22.js
+++ random codes/6-3-22.js
@@ -1,39 +1,19 @@
 // 6/3/22 variable errors
 
-var x = 1.5
-var y = true
-console.log(x + y) // 2.5?
-console.log(x * x * x) // 3.375, oklah
-console.log(y / x / x / x) //0.2962962962962963?, ok bagus
+var x = 1.5;
+var y = true;
+console.log(x + y); // 2.5?
+console.log(x * x * x); // 3.375, oklah
+console.log(y / x / x / x); //0.2962962962962963?, ok bagus
 
-var z = false
-var a = "anjai"
-console.log(z + a) // falseanjai?
+var z = false;
+var a = "anjai";
+console.log(z + a); // falseanjai?
 
-var b = true
-var c = false
-console.log(b * c + b / c) // null?
+var b = true;
+var c = false;
+console.log(b * c + b / c); // null?
 
-var d = "anjai"
-var e = true
-console.log(e * d) // null lagi
-
-// ganti console.log jadi print()
-
-var x = 1.5
-var y = true
-print(x + y) // 2.5?
-print(x * x * x) // 3.375, oklah
-print(y / x / x / x) //0.2962962962962963?
-
-var z = false
-var a = "anjai"
-print(z + a) // falseanjai?
-
-var b = true
-var c = false
-print(b * c + b / c) // null?
-
-var d = "anjai"
-var e = true
-print(e * d) // null lagi
+var d = "anjai";
+var e = true;
+console.log(e * d); // null lagi
